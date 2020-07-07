d2bc02fb74ab1a1eee56f3e64dce4d2023b39c54
SirVertar <mateusz.jakuszko@gmail.com>
2020-07-07 20:38:46 +0200

    first commit

---
+++ src/main/java/com/mateusz/jakuszko/roomforyoufront/RoomForYouFrontApplication.java
@@ -0,0 +1,13 @@
+package com.mateusz.jakuszko.roomforyoufront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomForYouFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomForYouFrontApplication.class, args);
+    }
+
+}
