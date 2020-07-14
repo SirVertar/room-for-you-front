4fa5760e4a87a5f90197c5ffff912eed318f9751
SirVertar <mateusz.jakuszko@gmail.com>
2020-07-14 21:54:53 +0200

    Version_0_0_1 - Added implementation for CustomerResponse and ReservationResponse

--- src/main/java/com/mateusz/jakuszko/roomforyoufront/roomforyouapi/response/CustomerResponse.java
+++ src/main/java/com/mateusz/jakuszko/roomforyoufront/roomforyouapi/response/CustomerResponse.java
@@ -1,4 +1,40 @@
 package com.mateusz.jakuszko.roomforyoufront.roomforyouapi.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "id",
+        "username",
+        "password",
+        "name",
+        "surname",
+        "email",
+        "role",
+        "reservations",
+        "apartmentDtos"
+})
 public class CustomerResponse {
+    @JsonProperty("id")
+    public Integer id;
+    @JsonProperty("username")
+    public String username;
+    @JsonProperty("password")
+    public String password;
+    @JsonProperty("name")
+    public String name;
+    @JsonProperty("surname")
+    public String surname;
+    @JsonProperty("email")
+    public String email;
+    @JsonProperty("role")
+    public String role;
+    @JsonProperty("reservations")
+    public List<ReservationResponse> reservations = null;
+    @JsonProperty("apartmentDtos")
+    public List<ApartmentResponse> apartmentDtos = null;
 }
--- src/main/java/com/mateusz/jakuszko/roomforyoufront/roomforyouapi/response/ReservationResponse.java
+++ src/main/java/com/mateusz/jakuszko/roomforyoufront/roomforyouapi/response/ReservationResponse.java
@@ -1,4 +1,26 @@
 package com.mateusz.jakuszko.roomforyoufront.roomforyouapi.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "id",
+        "startDate",
+        "endDate",
+        "apartmentId",
+        "userId"
+})
 public class ReservationResponse {
+    @JsonProperty("id")
+    public Integer id;
+    @JsonProperty("startDate")
+    public String startDate;
+    @JsonProperty("endDate")
+    public String endDate;
+    @JsonProperty("apartmentId")
+    public Integer apartmentId;
+    @JsonProperty("userId")
+    public Integer userId;
 }
