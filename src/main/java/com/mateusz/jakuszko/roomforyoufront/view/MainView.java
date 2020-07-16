d66880c2b98e1285adcdfcff4716ef4a8772ce97
SirVertar <mateusz.jakuszko@gmail.com>
2020-07-16 20:13:21 +0200

    Version_0_0_2 - Changed MainView class

--- src/main/java/com/mateusz/jakuszko/roomforyoufront/view/MainView.java
+++ src/main/java/com/mateusz/jakuszko/roomforyoufront/view/MainView.java
@@ -1,6 +1,7 @@
 package com.mateusz.jakuszko.roomforyoufront.view;
 
 import com.mateusz.jakuszko.roomforyoufront.dto.ApartmentDto;
+import com.mateusz.jakuszko.roomforyoufront.encoder.LongToStringEncoder;
 import com.mateusz.jakuszko.roomforyoufront.form.ApartmentForm;
 import com.mateusz.jakuszko.roomforyoufront.roomforyouapi.client.RoomForYouApartmentApiClient;
 import com.vaadin.flow.component.button.Button;
@@ -8,20 +9,62 @@ import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Route
-public class MainView2 extends VerticalLayout {
-    private Button logIn = new Button("Login");
-    private Button register = new Button("Register");
-    private Button apartment = new Button("Apartment");
-    private Button reservation = new Button("Reservation");
-    private final RoomForYouApartmentApiClient roomForYouApiClient;
+@Getter
+public class MainView extends VerticalLayout {
 
-    public MainView2(@Autowired RoomForYouApartmentApiClient roomForYouApiClient) {
+    private Button buttonLogIn = new Button("Login");
+    private Button buttonRegister = new Button("Register");
+    private Button buttonApartment = new Button("Apartment");
+    private Button buttonReservation = new Button("Reservation");
+    private Button buttonCustomer = new Button("Customer");
+    private final RoomForYouApartmentApiClient roomForYouApiClient;
+    private Grid apartmentGrid = new Grid<>(ApartmentDto.class);
+    private LongToStringEncoder longToStringEncoder;
+
+    public MainView(@Autowired RoomForYouApartmentApiClient roomForYouApiClient,
+                    @Autowired LongToStringEncoder longToStringEncoder) {
+        this.longToStringEncoder = longToStringEncoder;
         this.roomForYouApiClient = roomForYouApiClient;
-        HorizontalLayout mainContent = new HorizontalLayout(logIn, register, apartment, reservation);
+        addButtonsContent();
+        buttonApartment.addClickListener(event -> apartmentView());
+        buttonReservation.addClickListener(event -> reservationView());
+    }
+
+    public void addButtonsContent() {
+        HorizontalLayout buttonsContent = new HorizontalLayout(buttonLogIn, buttonRegister, buttonApartment,
+                buttonReservation, buttonCustomer);
+        buttonsContent.setAlignItems(Alignment.CENTER);
+        add(buttonsContent);
+    }
+
+    public void apartmentView() {
+        removeAll();
+        addButtonsContent();
+        ApartmentForm apartmentForm = new ApartmentForm(this, roomForYouApiClient, longToStringEncoder);
+        apartmentGrid.setColumns("city", "street", "streetNumber", "apartmentNumber", "id", "customerId");
+        HorizontalLayout mainContent = new HorizontalLayout(apartmentGrid, apartmentForm);
         mainContent.setSizeFull();
-        add(mainContent);
+        apartmentGrid.setSizeFull();
+        add(apartmentGrid, mainContent);
+        setSizeFull();
+        refresh();
+    }
+
+    public void refresh() {
+        apartmentGrid.setItems((roomForYouApiClient.getApartmentsResponse()));
+    }
+
+    public void reservationView() {
+        removeAll();
+        addButtonsContent();
+    }
+
+    public void customersView() {
+        removeAll();
+        addButtonsContent();
     }
 }
