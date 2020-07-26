47605dc7972c928873d7be46f263b71415d80d1e
SirVertar <mateusz.jakuszko@gmail.com>
2020-07-26 17:36:11 +0200

    Version_0_1_0 - changed application.properties, Main class and build.gradle for no production

--- src/main/java/com/mateusz/jakuszko/roomforyoufront/RoomForYouFrontApplication.java
+++ src/main/java/com/mateusz/jakuszko/roomforyoufront/RoomForYouFrontApplication.java
@@ -6,23 +6,10 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-//@SpringBootApplication
-//@EnableConfigurationProperties
-//public class RoomForYouFrontApplication {
-//    public static void main(String[] args) {
-//        SpringApplication.run(RoomForYouFrontApplication.class, args);
-//    }
-//}
-
 @SpringBootApplication
 @EnableConfigurationProperties
-public class RoomForYouFrontApplication extends SpringBootServletInitializer {
+public class RoomForYouFrontApplication {
     public static void main(String[] args) {
         SpringApplication.run(RoomForYouFrontApplication.class, args);
     }
-//
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        return application.sources(RoomForYouFrontApplication.class);
-//    }
 }
